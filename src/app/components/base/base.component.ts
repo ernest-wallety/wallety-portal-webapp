97bbbc311ce41b109d89a865fa5a103a06e5aea5
omoinjm <njmcloud@gmail.com>
2025-01-09 11:12:58 +0200

    begin with login process

---
+++ src/app/components/base/base.component.ts
@@ -0,0 +1,27 @@
+import { Directive, Injectable } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { AuthenticationHelper } from "../helpers/authentication_helper";
+import { LoginResultModel } from "../models/login_result";
+
+@Injectable()
+@Directive()
+export class BaseComponent {
+   public LoggedInUser: LoginResultModel;
+   public IsLoading: boolean = false;
+   public ViewModel: any;
+
+   // Get user detail every load and just blank populate the viewmodel
+   constructor(
+      // public data_service: DataService,
+      public router: Router,
+      public route: ActivatedRoute,
+      public toastr: ToastrService,
+      public ngbModalService: NgbModal,
+      // public lookupHelper: LookupHelper,
+   ) {
+      this.LoggedInUser = AuthenticationHelper.get_user_detail();
+      this.ViewModel = Object.assign(new Object());
+   }
+}
