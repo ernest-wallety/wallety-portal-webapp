90239e1fd49587a815328574c6a3f19703b9cd56
Nhlanhla Malaza <nhlanhla@wallety.cash>
2025-03-11 15:14:00 +0200

    working on testing

--- src/app/areas/auth/auth.component.spec.ts
+++ src/app/areas/auth/auth.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ActivatedRoute } from "@angular/router";
-import { beforeEach, describe } from "node:test";
+// import { beforeEach, describe } from "jasmine";
 import { AuthComponent } from "./auth.component";
 
 describe("AboutComponent", () => {
