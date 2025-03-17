5e0d04d7aa3b5e53d6c6905ec630438a6571d689
Nhlanhla Malaza <nhlanhla@wallety.cash>
2025-03-17 12:43:54 +0200

    adding to transactions screen

--- src/app/components/helpers/lookup_helper.ts
+++ src/app/components/helpers/lookup_helper.ts
@@ -36,6 +36,40 @@ export class LookupHelper {
     }
   }
 
+  // A means to populate lookup string parameter, from a multi lookup which controls an int array.
+  // There may be a more elegant way to override this change lookup function, but for now I am leaving it here as a separate function as to separate responsibility.
+  public static onChangeMultiLookup(
+    multiLookup: Lookup[],
+    listFieldName: string,
+  ): string {
+    // Find item index in the component's lookup list with corresponding field name.
+    const index = this.lookups.findIndex((item) => {
+      const isMatch = item.Name === this.transform(listFieldName);
+
+      console.log(item.Name);
+      console.log(listFieldName);
+
+      return isMatch;
+    });
+
+    if (index !== -1) {
+      // if item is found in the list then...
+
+      // Assign the array of id's to the corresponding lookup record.
+      this.lookups[index].IdArr = [];
+
+      multiLookup.forEach((part) => {
+        this.lookups[index].IdArr.push(part.Id ?? 0);
+      });
+
+      // Return new encoded json string with the all the lookups on the component appropriately populated.
+      return encodeURIComponent(JSON.stringify(this.lookups));
+    } else {
+      console.log("Lookup not found in array.");
+      return "";
+    }
+  }
+
   public static transform(value: string): string {
     if (!value) return "";
     const split = value.split(".");
